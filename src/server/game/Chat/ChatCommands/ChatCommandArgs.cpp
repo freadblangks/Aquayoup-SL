ac7ed7ecc9b64415cb5f277fba7a55a624bb5d1c
devovh <devovh@gmail.com>
2021-11-04 00:51:23 +0100

    Core/Update - Update to revision: a54c51c362426613be658e4a471b8fc866e82d57 Name: Core/DB: Rename SoundEntriesID to SoundKitID in BroadcastText

--- src/server/game/Chat/ChatCommands/ChatCommandArgs.cpp
+++ src/server/game/Chat/ChatCommands/ChatCommandArgs.cpp
@@ -17,7 +17,6 @@
 
 #include "ChatCommandArgs.h"
 #include "ChatCommand.h"
-#include "ChatCommandHyperlinks.h"
 #include "DB2Stores.h"
 #include "ObjectMgr.h"
 
@@ -26,7 +25,7 @@ using namespace Trinity::ChatCommands;
 struct AchievementVisitor
 {
     using value_type = AchievementEntry const*;
-    value_type operator()(Hyperlink<achievement> achData) const { return achData->achievement; }
+    value_type operator()(Hyperlink<achievement> achData) const { return achData->Achievement; }
     value_type operator()(uint32 achId) const { return sAchievementStore.LookupEntry(achId); }
 };
 char const* Trinity::ChatCommands::ArgInfo<AchievementEntry const*>::TryConsume(AchievementEntry const*& data, char const* args)
@@ -34,6 +33,20 @@ char const* Trinity::ChatCommands::ArgInfo<AchievementEntry const*>::TryConsume(
     Variant <Hyperlink<achievement>, uint32> val;
     if ((args = CommandArgsConsumerSingle<decltype(val)>::TryConsumeTo(val, args)))
         data = boost::apply_visitor(AchievementVisitor(), val);
+    return args;
+}
+
+struct CurrencyTypesVisitor
+{
+    using value_type = CurrencyTypesEntry const*;
+    value_type operator()(Hyperlink<currency> currency) const { return currency->Currency; }
+    value_type operator()(uint32 currencyId) const { return sCurrencyTypesStore.LookupEntry(currencyId); }
+};
+char const* Trinity::ChatCommands::ArgInfo<CurrencyTypesEntry const*>::TryConsume(CurrencyTypesEntry const*& data, char const* args)
+{
+    Variant <Hyperlink<currency>, uint32> val;
+    if ((args = CommandArgsConsumerSingle<decltype(val)>::TryConsumeTo(val, args)))
+        data = boost::apply_visitor(CurrencyTypesVisitor(), val);
     return args;
 }
 
--- src/server/game/Chat/ChatCommands/ChatCommandArgs.h
+++ src/server/game/Chat/ChatCommands/ChatCommandArgs.h
@@ -20,12 +20,13 @@
 
 #include "ChatCommandHelpers.h"
 #include "ChatCommandTags.h"
-#include "ChatCommandHyperlinks.h"
 
 struct GameTele;
 
-namespace Trinity {
-namespace ChatCommands {
+namespace Trinity
+{
+namespace ChatCommands
+{
 
 /************************** ARGUMENT HANDLERS *******************************************\
 |* Define how to extract contents of a certain requested type from a string             *|
@@ -115,6 +116,13 @@ struct TC_GAME_API ArgInfo<AchievementEntry const*>
     static char const* TryConsume(AchievementEntry const*&, char const*);
 };
 
+// CurrencyTypesEntry* from numeric id or link
+template <>
+struct TC_GAME_API ArgInfo<CurrencyTypesEntry const*>
+{
+    static char const* TryConsume(CurrencyTypesEntry const*&, char const*);
+};
+
 // GameTele* from string name or link
 template <>
 struct TC_GAME_API ArgInfo<GameTele const*>
@@ -129,6 +137,7 @@ struct TC_GAME_API ArgInfo<bool>
     static char const* TryConsume(bool&, char const*);
 };
 
-}}
+}
+}
 
 #endif
