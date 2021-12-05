a9d1cd2ae9b879c21f0ae190a4ee6da595bb820d
Juggalo187 <skulyspam@gmail.com>
2021-12-05 12:44:09 -0600

    Added Solocraft

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -67,6 +67,7 @@ void AddSC_REFORGER_NPC();
 void AddSC_TemplateNPC();
 void AddSC_NPC_Profession();
 void AddSC_Dynamic_Resurrections();
+void AddSolocraftScripts();
 
 void AddCustomScripts()
 {
@@ -117,5 +118,5 @@ void AddCustomScripts()
 	AddSC_TemplateNPC();
 	AddSC_NPC_Profession();
     AddSC_Dynamic_Resurrections();
-	
+	AddSolocraftScripts();
 }
