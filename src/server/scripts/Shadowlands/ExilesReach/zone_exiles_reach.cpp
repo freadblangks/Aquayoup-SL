62bdf75fc568db0fd70dee5dce5b63b7daacee83
zTerragor <81566364+zTerragor@users.noreply.github.com>
2021-12-16 02:52:35 +0500

    Core/Scripts Shadowlands initial scripts
    Core/Scripts Shadowlands initial scripts

--- src/server/scripts/Shadowlands/ExilesReach/zone_exiles_reach.cpp
+++ src/server/scripts/Shadowlands/ExilesReach/zone_exiles_reach.cpp
@@ -32,6 +32,22 @@
 #include "ZoneScript.h"
 #include "ScriptedGossip.h"
 
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Creature.h"
+#include "CreatureAI.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
+#include "AreaTriggerAI.h"
+#include "AreaTrigger.h"
+#include "InstanceScript.h"
+#include "CombatAI.h"
+#include "PhasingHandler.h"
+#include "TemporarySummon.h"
+#include "Log.h"
+#include "Chat.h"
+#include "ZoneScript.h"
+#include "ScriptedGossip.h"
 //160737 npc_combat_dummy
 struct npc_combat_dummy : public ScriptedAI
 {
@@ -40,16 +56,59 @@ struct npc_combat_dummy : public ScriptedAI
         SetCombatMovement(false);
         me->SetReactState(REACT_PASSIVE);
     }
-
     void JustDied(Unit* killer) override
     {
         if (Player* player = killer->ToPlayer())
             player->KilledMonsterCredit(174954);
+    }
+};
 
-           }
+enum ExileReach
+{
+    QUEST_STAND_YOUR_GROUND = 59927,
+};
+
+struct quest_stand_your_ground : public ScriptedAI
+{
+    quest_stand_your_ground(Creature* creature) : ScriptedAI(creature)
+    {
+        SetCombatMovement(false);
+    }
+
+    void Reset() override
+    {
+        ScriptedAI::Reset();
+    }
+
+    void QuestAccept(Player* player, Quest const* quest) override
+    {
+        if (quest->GetQuestId() == QUEST_STAND_YOUR_GROUND)
+        {
+            Talk(1);
+            me->SetFaction(14);
+            me->SetReactState(REACT_AGGRESSIVE);
+        }
+    }
+
+    void DamageTaken(Unit* attacker, uint32& /*damage*/) override
+    {
+        if (Player* player = attacker->ToPlayer())
+            if (me->GetHealth() < me->CountPctFromMaxHealth(20))
+                if (player->GetQuestStatus(QUEST_STAND_YOUR_GROUND))
+                {
+                    player->KilledMonsterCredit(155607);
+                }
+    }
+
+    void JustDied(Unit* killer) override
+    {
+        if (Player* player = killer->ToPlayer())
+                player->KilledMonsterCredit(155607);
+    }
 };
 
 void AddSC_zone_exiles_reach()
 {
     RegisterCreatureAI(npc_combat_dummy);
+    RegisterCreatureAI(quest_stand_your_ground);
 }
