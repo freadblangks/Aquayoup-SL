a3837a3d1ee7b2854753318bce94fee92d072808
welder1976 <zemetskia@gmail.com>
2021-12-26 23:23:26 +0300

    Up start quest

--- sql/updates/MagicStorm/start_questt_sl.sql
+++ sql/updates/MagicStorm/start_questt_sl.sql
@@ -213,3 +213,187 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnDifficult
 ,(@CGUID+20, @SHARD_OF_DOM_3, 2147, 10355, 10359, '0', 15699, 0, 0, 0, 504.348968505859375, -2124.229248046875, 844.4725341796875, 0.042546126991510391, 120, 0, 0, 7477, 0, 0, 0, 0, 0, 37474) -- 169100 (Area: The Frozen Throne - Difficulty: 0) (Auras: 328876 - [DNT] Shard of Domination 03)
 ,(@CGUID+21, 168670, 2147, 10355, 10359, '0', 15699, 0, 0, 0, 529.7100830078125, -2124.560791015625, 840.94024658203125, 0, 120, 0, 0, 11791, 0, 0, 0, 0, 0, 37474) -- 168670 (Area: The Frozen Throne - Difficulty: 0) (Auras: 328164 - [DNT] Reset Ping Aura)
 ;
+
+-- add areatrigger definition
+DELETE FROM `areatrigger_template` WHERE `Id` = @ATT_START+1 AND `IsServerSide` = 1;
+INSERT INTO `areatrigger_template` (`Id`, `IsServerSide`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
+(@ATT_START+1, 1, 1, 0, 20, 20, 20, 0, 0, 0, 'SmartAreaTriggerAI', 0);
+-- Add areatrigger (serverside, position)
+DELETE FROM `areatrigger` WHERE `SpawnId` = @AT_START+1;
+INSERT INTO `areatrigger` (`SpawnId`, `AreaTriggerId`, `IsServerSide`, `MapId`, `PosX`, `PosY`, `PosZ`, `Orientation`, `PhaseUseFlags`, `PhaseId`, `PhaseGroup`, `Comment`) VALUES
+(@AT_START+1, @ATT_START+1, 1, 2147, -642.88, 2208.25, 550.71, 0, 1, 0, 0, 'Chilling Summon Quest - Personal Summon Darion - Ebon Hold');
+-- Waypoitns for darion to walk
+DELETE FROM `waypoints` WHERE `entry` in (@WP_START+1, @WP_START+2, @WP_START+3);
+INSERT INTO `waypoints` (`entry`, `pointId`, `position_x`, `position_y`, `position_z`, `point_comment`) VALUES
+(@WP_START+1, 1, -637.0997, 2206.1636, 550.77936, 'SL Ebon Hold #1-1'),
+(@WP_START+1, 2, -628.3497, 2206.6636, 550.77936, 'SL Ebon Hold #1-2'),
+(@WP_START+1, 3, -625.5997, 2206.6636, 550.77936, 'SL Ebon Hold #1-3'),
+(@WP_START+1, 4, -603.5170, 2207.7400, 550.67940, 'SL Ebon Hold #1-4'),
+(@WP_START+2, 1, -599.09186, 2208.0654, 550.8266, 'SL Ebon Hold #2-1'),
+(@WP_START+2, 2, -598.84186, 2208.0654, 550.8266, 'SL Ebon Hold #2-2'),
+(@WP_START+2, 3, -598.09186, 2208.3154, 551.0766, 'SL Ebon Hold #2-3'),
+(@WP_START+2, 4, -595.84186, 2208.3154, 550.0766, 'SL Ebon Hold #2-4'),
+(@WP_START+2, 5, -592.59186, 2208.3154, 549.3266, 'SL Ebon Hold #2-5'),
+(@WP_START+2, 6, -588.34186, 2208.5654, 547.8266, 'SL Ebon Hold #2-6'),
+(@WP_START+2, 7, -587.34186, 2208.5654, 547.5766, 'SL Ebon Hold #2-7'),
+(@WP_START+2, 8, -583.34186, 2208.5654, 546.5766, 'SL Ebon Hold #2-8'),
+(@WP_START+2, 9, -582.84186, 2208.5654, 546.3266, 'SL Ebon Hold #2-9'),
+(@WP_START+2, 10, -578.34186, 2208.8154, 544.8266, 'SL Ebon Hold #2-10'),
+(@WP_START+2, 11, -576.84186, 2208.8154, 544.5766, 'SL Ebon Hold #2-11'),
+(@WP_START+2, 12, -575.09186, 2208.8154, 543.8266, 'SL Ebon Hold #2-12'),
+(@WP_START+2, 13, -574.34186, 2208.8154, 543.8266, 'SL Ebon Hold #2-13'),
+(@WP_START+2, 14, -574.09186, 2208.8154, 543.5766, 'SL Ebon Hold #2-14'),
+(@WP_START+2, 15, -571.59186, 2209.0654, 542.8266, 'SL Ebon Hold #2-15'),
+(@WP_START+2, 16, -568.34186, 2209.0654, 541.8266, 'SL Ebon Hold #2-16'),
+(@WP_START+2, 17, -567.84186, 2209.0654, 541.8266, 'SL Ebon Hold #2-17'),
+(@WP_START+2, 18, -565.59186, 2209.3154, 540.8266, 'SL Ebon Hold #2-18'),
+(@WP_START+2, 19, -565.09186, 2209.3154, 540.5766, 'SL Ebon Hold #2-19'),
+(@WP_START+2, 20, -562.16670, 2209.3906, 539.4738, 'SL Ebon Hold #2-20'),
+(@WP_START+3, 1, -562.16670, 2209.5212, 540.54694, 'SL Ebon Hold #3-1'),
+(@WP_START+3, 2, -561.8701, 2209.5212, 539.79694, 'SL Ebon Hold #3-2'),
+(@WP_START+3, 3, -560.3701, 2209.7712, 539.79694, 'SL Ebon Hold #3-3'),
+(@WP_START+3, 4, -559.8701, 2210.0212, 540.04694, 'SL Ebon Hold #3-4'),
+(@WP_START+3, 5, -559.6201, 2210.0212, 539.79694, 'SL Ebon Hold #3-5'),
+(@WP_START+3, 6, -558.6201, 2210.0212, 539.79694, 'SL Ebon Hold #3-6'),
+(@WP_START+3, 7, -557.3701, 2210.2712, 539.79694, 'SL Ebon Hold #3-7'),
+(@WP_START+3, 8, -549.5226, 2211.2935, 539.49854, 'SL Ebon Hold #3-8');
+-- Smart scripts: summon personal spawns
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @AT_START+1 AND `source_type` = 12;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@AT_START+1, 12, 0, 1, 46, 0, 100, 0, 0, 0, 0, 0, 0, 12, @DARION_EBON_HOLD_ENTRY, 8, 0, 0, 3, 0, 8, 0, 0, 0, -642.6823, 2205.587, 550.87933, 6.180687427520751953, "On areatriggerserver - summondarion, personal spawn");
+-- Smart scripts: On darion spawn
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @DARION_EBON_HOLD_ENTRY AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@DARION_EBON_HOLD_ENTRY, 0, 0, 1, 11, 0, 100, 0, 1, 2147, 0, 0, 0, 11,  @SPELL_SHADOW_TELEPORT, 2,           0xFFFFFFFF, 0,      0, 0,  1, 0, 0, 0, 0, 0, 0, 0, "On respawn - cast shadow teleport"),
+(@DARION_EBON_HOLD_ENTRY, 0, 1, 2, 61, 0, 100, 0, 0, 0,    0, 0, 0, 53,  0,                      @WP_START+1, 0,          @QUEST, 0, 0,  1, 0, 0, 0, 0, 0, 0, 0, "On link - start waypoint"),
+(@DARION_EBON_HOLD_ENTRY, 0, 2, 7, 61, 0, 100, 0, 0, 0,    0, 0, 0, 143, @CONVT_START+2,         1,           0,          0,      0, 0, 23, 0, 0, 0, 0, 0, 0, 0, "On link - start talking"),
+(@DARION_EBON_HOLD_ENTRY, 0, 3, 7, 58, 0, 100, 0, 0, @WP_START+1, 0, 0, 0, 53, 0, @WP_START+2, 0, @QUEST, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "On waypoint #1 end reached - start waypoint #2"), -- todo: start waypoint #2 on conversation ended instead
+(@DARION_EBON_HOLD_ENTRY, 0, 4, 7, 58, 0, 100, 0, 0, @WP_START+2, 0, 0, 0, 53, 0, @WP_START+3, 0, @QUEST, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "On waypoint #2 end reached - start waypoint #3"),
+(@DARION_EBON_HOLD_ENTRY, 0, 5, 6, 58, 0, 100, 0, 0, @WP_START+3, 0, 0, 0, 11, @SPELL_SHADOW_TELEPORT, 2, 0xFFFFFFFF, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "On waypoint #3 end reached - cast shadow teleport"),*/
+-- (@DARION_EBON_HOLD_ENTRY, 0, 6, 7, 61 /* link */, 0, 100, 0, 0, 0, 0, 0, 0, 41 /* despawn */, 0, 0, 0, 0, 0, 0, 1 /* target: self */, 0, 0, 0, 0, 0, 0, 0, "On link -- despawn");
+
+-- Conditions: only summon personal spawn if not summoned already
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceGroup` IN (1, 3) AND `SourceEntry` = @AT_START+1 AND `SourceId` = 12;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
+(22, 1, @AT_START+1, 12, 1, 29, 0, @DARION_EBON_HOLD_ENTRY, 110, 0, 1, 0, '', "Script 1: AND summon once - we are not near darion. We use distance higher than areatrigger to make sure we only trigger this once");
+-- Conversation
+DELETE FROM `conversation_template` WHERE `Id` IN (@CONVT_START+2);
+INSERT INTO `conversation_template` (`Id`, `FirstLineID`, `LastLineEndTime`, `TextureKitId`, `VerifiedBuild`) VALUES
+(@CONVT_START+2, 37489, 39617, 0, 37474);
+DELETE FROM `conversation_line_template` WHERE `Id` IN (37489, 37490, 37491, 39251, 37492);
+INSERT INTO `conversation_line_template` (`Id`, `StartTime`, `UiCameraID`, `ActorIdx`, `Flags`, `VerifiedBuild`) VALUES
+(37489, 0, 0, 0, 0, 37474),
+(37490, 11444, 0, 0, 0, 37474),
+(37491, 18404, 0, 0, 0, 37474),
+(39251, 27939, 0, 0, 0, 37474),
+(37492, 33094, 0, 0, 0, 37474);
+-- ------------------------- Part 5: Scourge Transporter - from Deathbringer's Rise to The Frozen Throne
+-- Add areatrigger template (box definition)
+DELETE FROM `areatrigger_template` WHERE `Id` = @ATT_START+2 AND `IsServerSide` = 1;
+INSERT INTO `areatrigger_template` (`Id`, `IsServerSide`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
+(@ATT_START+2, 1, 1, 0, 3.6, 3.442, 2, 0, 0, 0, '', 0);
+-- Add areatrigger (serverside, position)
+DELETE FROM `areatrigger` WHERE `SpawnId` = @AT_START+2;
+INSERT INTO `areatrigger` (`SpawnId`, `AreaTriggerId`, `IsServerSide`, `MapId`, `PosX`, `PosY`, `PosZ`, `Orientation`, `PhaseUseFlags`, `PhaseId`, `PhaseGroup`, `Comment`) VALUES
+(@AT_START+2, @ATT_START+2, 1, 2147, -548.971923828125, 2211.219970703125, 539.2769775390625, 0, 1, 0, 0, 'Deathbringer\'s Rise - Teleport to The Frozen Throne');
+-- Add area trigger action to cast the teleport spell
+DELETE FROM areatrigger_template_actions WHERE `AreaTriggerId` = @AT_START+2 AND `IsServerSide` = 1;
+INSERT INTO areatrigger_template_actions (`AreaTriggerId`, `IsServerSide`, `ActionType`, `ActionParam`, `TargetType`) VALUES
+(@AT_START+2, 1, 0, @SPELL_TELEPORT_FROZEN_THRONE, 5);
+-- Teleporter spell target position
+DELETE FROM `spell_target_position` WHERE `id` = @SPELL_TELEPORT_FROZEN_THRONE;
+INSERT INTO `spell_target_position` (`id`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`) VALUES
+(@SPELL_TELEPORT_FROZEN_THRONE, 0, 2147, 559.020, -2124.869, 840.857); 
+-- ------------------------- Part 6: Deathbringer's Rise fall-teleport area trigger
+-- Add areatrigger template (box definition)
+DELETE FROM `areatrigger_template` WHERE `Id` = @ATT_START+3 AND `IsServerSide` = 1;
+INSERT INTO `areatrigger_template` (`Id`, `IsServerSide`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
+(@ATT_START+3, 1, 1, 0, 1000, 1000, 20, 0, 0, 0, 'SmartAreaTriggerAI', 0);
+  
+-- Add areatrigger (serverside, position)
+DELETE FROM `areatrigger` WHERE `SpawnId` = @AT_START+3;
+INSERT INTO `areatrigger` (`SpawnId`, `AreaTriggerId`, `IsServerSide`, `MapId`, `PosX`, `PosY`, `PosZ`, `Orientation`, `PhaseUseFlags`, `PhaseId`, `PhaseGroup`, `Comment`) VALUES
+(@AT_START+3, @ATT_START+3, 1, 2147, -584.556702, 2211.174805, 516.24445, 0, 1, 0, 0, 'Deathbringer\'s Rise - Falling Teleporter'); -- alliance
+ 
+-- Add area trigger action to cast the teleport spell
+DELETE FROM areatrigger_template_actions WHERE `AreaTriggerId` = @AT_START+3 AND `IsServerSide` = 1;
+INSERT INTO areatrigger_template_actions (`AreaTriggerId`, `IsServerSide`, `ActionType`, `ActionParam`, `TargetType`) VALUES
+(@AT_START+3, 1, 0, @SPELL_TELEPORT_ACHERUS, 5);
+-- ------------------------- Part 7: The Frozen Throne
+-- Add areatrigger template (box definition)
+DELETE FROM `areatrigger_template` WHERE `Id` IN (@ATT_START+4, @ATT_START+5) AND `IsServerSide` = 1;
+INSERT INTO `areatrigger_template` (`Id`, `IsServerSide`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
+(@ATT_START+4, 1, 1, 0, 50, 50, 50, 0, 0, 0, 'SmartAreaTriggerAI', 0),
+(@ATT_START+5, 1, 0, 0, 25, 25, 0,  0, 0, 0, 'SmartAreaTriggerAI', 0);
+-- Add areatrigger (serverside, position)
+DELETE FROM `areatrigger` WHERE `SpawnId` IN (@AT_START+4, @AT_START+5, @AT_START+6);
+INSERT INTO `areatrigger` (`SpawnId`, `AreaTriggerId`, `IsServerSide`, `MapId`, `PosX`, `PosY`, `PosZ`, `Orientation`, `PhaseUseFlags`, `PhaseId`, `PhaseGroup`, `Comment`) VALUES
+(@AT_START+4, @ATT_START+3, 1, 2147, 506.200, -2123.782, 816.4809, 0, 1, 0, 0, 'SL The Frozen Throne - Falling Teleporter'),
+(@AT_START+5, @ATT_START+4, 1, 2147, 559.020, -2124.869, 840.857, 0, 1, 0, 0, 'SL The Frozen Throne - Summon personal spawns');
+-- TODO after we have the back-teleporter position (@AT_START+6, @ATT_START+2, 1, 2147, x, y, z, 0, 1, 0, 0, 'SL The Frozen Throne - Back Teleporter (to Deathbringer\'s rise)');
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @AT_START+5 AND `source_type` = 12;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@AT_START+5, 12, 0, 1, 46, 0, 100, 0, 0, 0, 0, 0, 12, @BOLVAR_ENTRY,  8, 0, 0, 3, 0, 8, 0, 0, 0, 500.71182, -2127.2188, 840.9403, 0.647, "On areatriggerserver - summon high inquisitor, personal spawn"),
+(@AT_START+5, 12, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 12, @GENN_ENTRY,    8, 0, 0, 3, 0, 8, 0, 0, 0, 507.8559, -2119.361, 840.9403, 3.925928, "On link - summon Genn Greymane, personal spawn"),
+(@AT_START+5, 12, 2, 3, 61, 0, 100, 0, 0, 0, 0, 0, 12, @TYRANDE_ENTRY, 8, 0, 0, 3, 0, 8, 0, 0, 0, 510.42883, -2121.1355, 840.94025, 3.416774, "On link - summon Tyrande Whisperwind, personal spawn"),
+(@AT_START+5, 12, 3, 4, 61, 0, 100, 0, 0, 0, 0, 0, 12, @LTT_ENTRY,     8, 0, 0, 3, 0, 8, 0, 0, 0, 510.51562, -2126.4722, 840.94025, 2.71547, "On link - summon Lor'themar Theron, personal spawn"),
+(@AT_START+5, 12, 4, 5, 61, 0, 100, 0, 0, 0, 0, 0, 12, @CALIA_ENTRY,   8, 0, 0, 3, 0, 8, 0, 0, 0, 509.1389, -2128.3665, 840.94025, 2.512369, "On link - summon Calia Menethil, personal spawn");
+-- Add area trigger action to cast the teleport spell
+DELETE FROM areatrigger_template_actions WHERE `AreaTriggerId` IN (@AT_START+3, @AT_START+6) AND `IsServerSide` = 1;
+INSERT INTO areatrigger_template_actions (`AreaTriggerId`, `IsServerSide`, `ActionType`, `ActionParam`, `TargetType`) VALUES
+(@AT_START+4, 1, 0, @SPELL_TELEPORT_FROZEN_THRONE, 5), -- on falling - go to frozen throne
+(@AT_START+6, 1, 0, @SPELL_TELEPORT_ICECROWN_LOWER, 5); -- on back teleporter
+-- Teleporter spell target position
+DELETE FROM `spell_target_position` WHERE `id` = @SPELL_TELEPORT_FROZEN_THRONE;
+INSERT INTO `spell_target_position` (`id`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`) VALUES
+(@SPELL_TELEPORT_ICECROWN_LOWER, 0, 2147, -555.72, 2211.21, 539.34); 
+-- ------------------------- Part 8: The frozen throne - talk
+UPDATE `creature_template` SET `AIName` = "SmartAI" where `entry` IN (@LTT_ENTRY);
+-- Smart scripts: Talk when player is near LTT_ENTRY (25 yards)
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @LTT_ENTRY AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+ (@LTT_ENTRY, 0, 0, 1, 10, 0, 100, 1, 1, 25, 0, 0, 1, 143, @CONVT_START+3, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On near darion - start talking"),
+
+-- Conversation 198459 
+
+DELETE FROM `conversation_template` WHERE `Id` IN (@CONVT_START+2);
+INSERT INTO `conversation_template` (`Id`, `FirstLineID`, `LastLineEndTime`, `TextureKitId`, `VerifiedBuild`) VALUES
+(@CONVT_START+3, 37351, 73480, 0, 37474);
+DELETE FROM `conversation_line_template` WHERE `Id` IN (37351, 37352, 37507, 37353, 37354, 37516, 37355, 38344, 38345, 38346, 38347,
+38348, 38349, 38350, 37508, 37356, 37357, 38351, 38352, 38353, 37509, 38354, 38355, 38356, 37510, 37511, 37512, 37358);
+INSERT INTO `conversation_line_template` (`Id`, `StartTime`, `UiCameraID`, `ActorIdx`, `Flags`, `VerifiedBuild`) VALUES
+(37351, 0, 0, 0, 0, 37474),		-- LTT
+(37352, 10481, 0, 0, 0, 37474), -- LTT
+(37507, 20074, 0, 1, 0, 37474), -- Whisperwind
+(37353, 28571, 0, 1, 1, 37474), -- Whsperwind (empty text)
+(37354, 28571, 0, 2, 1, 37474), -- Genn Greymane
+(37516, 39843, 0, 0, 1, 37474), -- LTT (empty text)
+(37355, 39843, 0, 3, 1, 37474),
+(38344, 47108, 0, 0, 1, 37474),
+(38345, 47108, 0, 4, 1, 37474),
+(38346, 47108, 0, 5, 1, 37474),
+(38347, 47108, 0, 6, 1, 37474),
+(38348, 47108, 0, 2, 1, 37474),
+(38349, 47108, 0, 7, 1, 37474),
+(38350, 47108, 0, 8, 1, 37474),
+(37508, 47108, 0, 9, 1, 37474),
+(37356, 47108, 0, 1, 1, 37474),
+(37357, 55711, 0, 10, 0, 37474),
+(38351, 65221, 0, 0, 1, 37474),
+(38352, 65221, 0, 4, 1, 37474),
+(38353, 65221, 0, 6, 1, 37474),
+(37509, 65221, 0, 5, 1, 37474),
+(38354, 65221, 0, 2, 1, 37474),
+(38355, 65221, 0, 7, 1, 37474),
+(38356, 65221, 0, 8, 1, 37474),
+(37510, 65221, 0, 9, 1, 37474),
+(37511, 65221, 0, 3, 1, 37474),
+(37512, 65221, 0, 1, 1, 37474),
+(37358, 65221, 0, 10, 0, 37474);
+DELETE FROM `conversation_actor_template` WHERE `Id` IN (@CONVAT_START+1);
+INSERT INTO `conversation_actor_template` (`Id`, `CreatureId`, `CreatureModelId`, `VerifiedBuild`) VALUES
+(@CONVAT_START+1, 165918, 27153, 0);
+DELETE FROM `conversation_actors` WHERE `ConversationId` IN (@CONVT_START+4);
+INSERT INTO `conversation_actors` (`ConversationId`, `ConversationActorId`, `ConversationActorGuid`, `Idx`, `VerifiedBuild`) VALUES
+(@CONVT_START+4, @CONVAT_START+0, 0, 0, 0);
