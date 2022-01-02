8e439135ff3816c55bdfe8d8283195f600005492
welder1976 <zemetskia@gmail.com>
2022-01-02 21:09:37 +0300

    start fix zone_exiles_reach

---
+++ src/server/scripts/Shadowlands/ExilesReach/zone_exiles_reach.cpp
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Creature.h"
+#include "CreatureAI.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
+#include "AreaTriggerAI.h"
+#include "AreaTrigger.h"
+#include "InstanceScript.h"
+#include "CombatAI.h"
+#include "PhasingHandler.h"
+#include "TemporarySummon.h"
+#include "Log.h"
+#include "Chat.h"
+#include "ScriptedGossip.h"
+
+//160737 npc_combat_dummy
+struct npc_combat_dummy : public ScriptedAI
+{
+    npc_combat_dummy(Creature* creature) : ScriptedAI(creature)
+    {
+        SetCombatMovement(false);
+        me->SetReactState(REACT_PASSIVE);
+    }
+
+    void JustDied(Unit* killer) override
+    {
+        if (Player* player = killer->ToPlayer())
+            player->KilledMonsterCredit(174954);
+    }
+};
+
+void AddSC_zone_exiles_reach()
+{
+    RegisterCreatureAI(npc_combat_dummy);
+}
