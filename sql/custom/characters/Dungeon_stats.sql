81ad520b57d5be3b476720a2f348a0944a38f103
Juggalo187 <skulyspam@gmail.com>
2022-02-25 11:22:57 -0600

    group aoe loot, stat removal sql,dungeon stat sql

---
+++ sql/custom/characters/Dungeon_stats.sql
@@ -0,0 +1,28 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping structure for table characters.stats_from_dungeons
+DROP TABLE IF EXISTS `stats_from_dungeons`;
+CREATE TABLE `stats_from_dungeons` (
+  `GUID` int(10) unsigned NOT NULL,
+  `Agility` int(11) NOT NULL DEFAULT '0',
+  `Intellect` int(11) NOT NULL DEFAULT '0',
+  `Spirit` int(11) NOT NULL DEFAULT '0',
+  `Strength` int(11) NOT NULL DEFAULT '0',
+  `Stamina` int(11) NOT NULL DEFAULT '0',
+  `Spellbonus` int(11) NOT NULL DEFAULT '0',
+  `AttackPower` int(11) NOT NULL DEFAULT '0',
+  `RAttackPower` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`GUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
