b8e848e025f45f7d7668be5fd672d874798fc7c4
KamiliaBlow#2908 <windowstar24to7@gmail.com>
2022-12-19 02:54:57 +0900

    newermind

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -4343,6 +4343,9 @@ void ObjectMgr::LoadPlayerInfo()
                 if (sWorld->getIntConfig(CONFIG_EXPANSION) < EXPANSION_LEGION && class_ == CLASS_DEMON_HUNTER)
                     continue;
 
+                if (sWorld->getIntConfig(CONFIG_EXPANSION) < EXPANSION_DRAGONFLIGHT && class_ == CLASS_EVOKER && (race == RACE_DRACTHYR_ALLIANCE || race == RACE_DRACTHYR_HORDE))
+                    continue;
+
                 // fatal error if no level 1 data
                 if (!playerInfo->get()->levelInfo || playerInfo->get()->levelInfo[0].stats[0] == 0)
                 {
