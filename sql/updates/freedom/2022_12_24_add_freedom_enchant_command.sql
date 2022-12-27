a5bf7240887821bb63975b8c002b500ec2d4b071
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2022-12-27 20:44:24 +0100

    Fix error in sql files

--- sql/updates/freedom/2022_12_24_add_freedom_enchant_command.sql
+++ sql/updates/freedom/2022_12_24_add_freedom_enchant_command.sql
@@ -1,4 +1,4 @@
 -- FREEDOM CUSTOM COMMANDS
-REPLACE INTO `commands` (command, syntax, description, gmlevel) VALUESVALUES
+REPLACE INTO `commands` (command, syntax, description, gmlevel) VALUES
 ('freedom enchant', 'Syntax: .freedom enchant $enchantId', 'Applies the enchantment with id $enchantId to the weapon in your main hand slot.', 0)
 ;
