8479a76c2631ed8a9a1375171aa35a7ba16b43ae
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2022-12-27 20:26:51 +0100

    Add SQL files for freedom_web commands

---
+++ sql/updates/freedom/2022_12_25_add_freedom_builder_commands.sql
@@ -0,0 +1,5 @@
+-- FREEDOM CUSTOM COMMANDS
+REPLACE INTO `commands` (command, syntax, description, gmlevel) VALUES
+('gobject clone', 'Syntax: .gobject clone', 'Creates a clone of the targetted gameobject.', 1),
+('go gobtarget', 'Syntax: .go gobtarget', 'Teleports you to your targgeted gameobject.', 1)
+;
