558084a77511aebd108caa52940d1a1571ed8a16
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2023-02-06 21:24:18 +0100

    Minor fixes to previous sql scripts

--- sql/custom/world/2023_02_06_add_marianna_npc.sql
+++ sql/custom/world/2023_02_06_add_marianna_npc.sql
@@ -1,10 +1,10 @@
 REPLACE INTO `npc_text` (ID, Probability0, BroadcastTextID0)
 VALUES (1000000,1,222000);
 
-REPLACE INTO `9x_world`.`gossip_menu` (MenuId, TextId)
+REPLACE INTO `gossip_menu` (MenuId, TextId)
 VALUES (60000,1000000);
 
-REPLACE INTO `creature_template` (`entry`, `faction`, `name`, `minlevel`, `maxlevel`, `gossip_menu_id`, `npcflags`)
+REPLACE INTO `creature_template` (`entry`, `faction`, `name`, `minlevel`, `maxlevel`, `gossip_menu_id`, `npcflag`)
 VALUES (399999, 35, 'Marianna Bisen', 100, 100, 60000, 1);
 
 REPLACE INTO `creature_template_model` (`CreatureID`, `CreatureDisplayID`)
