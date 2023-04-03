6c815a7ab8b1015c7f439e266a312ffc3aa1205a
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2023-04-03 11:45:31 +0200

    Remove problematic gobs

---
+++ sql/custom/world/2023_04_03_remove_000_and_001_gobs.sql
@@ -0,0 +1,8 @@
+DELETE FROM gameobject_template
+WHERE NAME LIKE '%000%.wmo';
+
+DELETE FROM gameobject_template
+WHERE NAME LIKE '%001%.wmo';
+
+DELETE FROM gameobject_template
+WHERE NAME LIKE '%001.m2';
