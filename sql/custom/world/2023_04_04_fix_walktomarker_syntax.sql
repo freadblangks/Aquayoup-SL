178466b9256b0b67ac81f653ef70bccc3548a957
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2023-04-04 18:58:18 +0200

    Fix .walktomarker help syntax

---
+++ sql/custom/world/2023_04_04_fix_walktomarker_syntax.sql
@@ -0,0 +1,5 @@
+-- FREEDOM CUSTOM COMMANDS
+REPLACE INTO `command` (name, help) VALUES
+('walktomarker', 'Syntax: .walktomarker\nTells your currently targetted NPC to walk to the location where you placed your marker with the marker spell.'),
+('walktomarker formation', 'Syntax: .walktomarker formation $formationKey\nTells formation with $formationKey to walk to the location where you placed your marker with the marker spell.')
+;
