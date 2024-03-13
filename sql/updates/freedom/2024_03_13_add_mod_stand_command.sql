bd20beabe634de4fea0f2af7bc204b2ed1560738
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2024-03-13 19:42:23 +0100

    Add .mod standstate to command list

---
+++ sql/updates/freedom/2024_03_13_add_mod_stand_command.sql
@@ -0,0 +1,4 @@
+-- FREEDOM CUSTOM COMMANDS
+REPLACE INTO `commands` (command, syntax, description, gmlevel) VALUES
+('modify standstate', 'Syntax: .modify standstate #emoteid','Change the emote of your character while standing to #emoteid.',0)
+;
